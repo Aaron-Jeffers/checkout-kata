0d94322395b568e113c2c55d8d24ef40b3fc63fb
Aaron-Jeffers <47363247+Aaron-Jeffers@users.noreply.github.com>
2026-05-19 21:40:21 +0100

    Created empty Unit Tests for Checkout.Scan()

--- Checkout-Kata.tests/CheckoutTests.cs
+++ Checkout-Kata.tests/CheckoutTests.cs
@@ -3,7 +3,18 @@ namespace Checkout_Kata.tests
     public class CheckoutTests
     {
         [Fact]
-        public void SampleTestThatShouldFail()
+        public void ScanningAnNullStringShouldThrowException()
+        {
+            Assert.Fail("This is a sample test that should fail.");
+        }
+        [Fact]
+        public void ScanningAnEmptyStringShouldThrowException()
+        {
+            Assert.Fail("This is a sample test that should fail.");
+        }
+
+        [Fact]
+        public void ScanningAWhiteSpaceStringShouldThrowException()
         {
             Assert.Fail("This is a sample test that should fail.");
         }
