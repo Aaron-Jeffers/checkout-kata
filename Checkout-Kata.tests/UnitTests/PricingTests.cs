d83895b461016f6df9b033efe4d0cd89d005b7b9
Aaron-Jeffers <47363247+Aaron-Jeffers@users.noreply.github.com>
2026-05-19 22:25:50 +0100

    Add placeholder pricing unit tests

---
+++ Checkout-Kata.tests/UnitTests/PricingTests.cs
@@ -0,0 +1,41 @@
+﻿namespace Checkout_Kata.tests.UnitTests
+{
+    public class PricingTests
+    {
+        [Fact]
+        public void GetTotalPrice_NoItems_ReturnZero()
+        {
+            Assert.Fail("This is a sample test that should fail.");
+        }
+
+        [Fact]
+        public void GetTotalPrice_OneItem_CorrectPricing()
+        {
+            Assert.Fail("This is a sample test that should fail.");
+        }
+
+        [Fact]
+        public void GetTotalPrice_MultipleItems_CorrectPricing()
+        {
+            Assert.Fail("This is a sample test that should fail.");
+        }
+
+        [Fact]
+        public void GetTotalPrice_OfferItems_CorrectPricing()
+        {
+            Assert.Fail("This is a sample test that should fail.");
+        }
+
+        [Fact]
+        public void GetTotalPrice_MixedItemsAndOfferItems_CorrectPricing()
+        {
+            Assert.Fail("This is a sample test that should fail.");
+        }
+
+        [Fact]
+        public void GetTotalPrice_ExampleCheckoutScenario_CorrectPricing()
+        {
+            Assert.Fail("This is a sample test that should fail.");
+        }
+    }
+}
