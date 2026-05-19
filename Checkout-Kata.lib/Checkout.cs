4cfbfe38b6d7074c02f88a53542f6e508a498d3d
Aaron-Jeffers <47363247+Aaron-Jeffers@users.noreply.github.com>
2026-05-19 19:46:07 +0100

    Created barebones library project and classes.

---
+++ Checkout-Kata.lib/Checkout.cs
@@ -0,0 +1,15 @@
+﻿namespace Checkout_Kata.lib
+{
+    public class Checkout : ICheckout
+    {
+        public int GetTotalPrice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(string item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Checkout-Kata.lib/ICheckout.cs
@@ -0,0 +1,8 @@
+﻿namespace Checkout_Kata.lib
+{
+    internal interface ICheckout
+    {
+        void Scan(string item);
+        int GetTotalPrice();
+    }
+}
