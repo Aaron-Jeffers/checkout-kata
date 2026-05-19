9047a8eb5d7167bbca5d5d12a65bc65203987c10
Aaron-Jeffers <47363247+Aaron-Jeffers@users.noreply.github.com>
2026-05-19 20:38:17 +0100

    Created pricing classes

---
+++ Checkout-Kata.lib/Pricing/ItemPrice.cs
@@ -0,0 +1,17 @@
+﻿namespace Checkout_Kata.lib.Pricing
+{
+    internal class ItemPrice
+    {
+        public ItemPrice(string sku, int unitPrice, SpecialPrice? specialPrice) 
+        {
+            SKU = sku;
+            UnitPrice = unitPrice;
+            SpecialPrice = specialPrice;
+        }
+        public string SKU { get; }
+
+        public int UnitPrice { get; }
+
+        public SpecialPrice? SpecialPrice { get; }
+    }
+}
---
+++ Checkout-Kata.lib/Pricing/SpecialPrice.cs
@@ -0,0 +1,13 @@
+﻿namespace Checkout_Kata.lib.Pricing
+{
+    internal class SpecialPrice
+    {
+        public SpecialPrice(int quantity, int price) 
+        {
+            Quantity = quantity;
+            Price = price;
+        }
+        public int Quantity { get; }
+        public int Price { get; }
+    }
+}
