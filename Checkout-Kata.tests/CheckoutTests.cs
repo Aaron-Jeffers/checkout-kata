2d27acf302de38d8ccf7d019fabc89f23f5975da
Aaron-Jeffers <47363247+Aaron-Jeffers@users.noreply.github.com>
2026-05-19 20:11:21 +0100

    Created barebones test project and class.

---
+++ Checkout-Kata.tests/CheckoutTests.cs
@@ -0,0 +1,11 @@
+namespace Checkout_Kata.tests
+{
+    public class CheckoutTests
+    {
+        [Fact]
+        public void SampleTestThatShouldFail()
+        {
+            Assert.Fail("This is a sample test that should fail.");
+        }
+    }
+}
