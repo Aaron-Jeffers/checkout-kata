86a841e1c341680473715b2bf71f49c655eacee8
Aaron-Jeffers <47363247+Aaron-Jeffers@users.noreply.github.com>
2026-05-19 21:55:42 +0100

    Created Exception to handle empty item price list for the Checkout constructor.

---
+++ Checkout-Kata.lib/Exceptions/PriceListIsEmptyException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkout_Kata.lib.Exceptions
+{
+    internal class PriceListIsEmptyException : Exception
+    {
+        public PriceListIsEmptyException() : base("Price list cannot be empty") { }
+    }
+}
