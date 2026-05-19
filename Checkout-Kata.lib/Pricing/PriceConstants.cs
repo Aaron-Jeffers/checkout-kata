f48b61af431eb67c16ec2c9b1e13c236c6897c56
Aaron-Jeffers <47363247+Aaron-Jeffers@users.noreply.github.com>
2026-05-19 20:49:57 +0100

    Created static PriceConstants class to hold pricing data for each individual SKU

---
+++ Checkout-Kata.lib/Pricing/PriceConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace Checkout_Kata.lib.Pricing
+{
+    internal static class PriceConstants
+    {
+        public static List<ItemPrice> ItemPrices { get; } = new List<ItemPrice>
+        {
+            new ItemPrice("A", 50, new SpecialPrice(quantity: 3, price: 130)),
+            new ItemPrice("B", 30, new SpecialPrice(quantity: 2, price: 45)),
+            new ItemPrice("C", 20, null),
+            new ItemPrice("D", 15, null) 
+        };
+    }
+}
