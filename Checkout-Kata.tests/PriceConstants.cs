291b9967a436801959b22d7548001fad36555926
Aaron-Jeffers <47363247+Aaron-Jeffers@users.noreply.github.com>
2026-05-19 21:14:46 +0100

    Moved PriceConstants.cs to Test project

--- Checkout-Kata.tests/PriceConstants.cs
+++ Checkout-Kata.tests/PriceConstants.cs
@@ -1,8 +1,10 @@
-﻿namespace Checkout_Kata.lib.Pricing
+﻿using Checkout_Kata.lib.Pricing;
+
+namespace Checkout_Kata.tests
 {
     internal static class PriceConstants
     {
-        public static List<ItemPrice> ItemPrices { get; } = new List<ItemPrice>
+        internal static List<ItemPrice> ItemPrices { get; } = new List<ItemPrice>
         {
             new ItemPrice("A", 50, new SpecialPrice(quantity: 3, price: 130)),
             new ItemPrice("B", 30, new SpecialPrice(quantity: 2, price: 45)),
